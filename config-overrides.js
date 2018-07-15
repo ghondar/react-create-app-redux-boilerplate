6d2dbcc779ce8bbf9bb196d546cb99476c3d8fdf
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2018-07-15 02:38:29 -0500

    [ ADD ] production optimization and update packages

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,10 @@
 const DashboardPlugin = require('webpack-dashboard/plugin')
 const { _moduleAliases } = require('./package.json')
+const rewireVendorSplitting = require('react-app-rewire-vendor-splitting')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const paths = require('react-app-rewired/scripts/utils/paths')
 const { injectBabelPlugin } = require('react-app-rewired')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
 
@@ -18,19 +22,14 @@ function getLoader(rules, matcher) {
   let match
 
   rules.some(rule => {
-    return (match = matcher(rule) ?
-      { rules, rule } :
-      getLoader(rule.use || rule.oneOf || [], matcher))
+    return (match = matcher(rule) ? { rules, rule } : getLoader(rule.use || rule.oneOf || [], matcher))
   })
 
   return match
 }
 
 function rewireStylus(config, env) {
-  const { rule: cssRule, rules } = getLoader(
-    config.module.rules,
-    rule => String(rule.test) === String(/\.css$/)
-  )
+  const { rule: cssRule, rules } = getLoader(config.module.rules, rule => String(rule.test) === String(/\.css$/))
 
   const stylusRule = deepClone(cssRule)
 
@@ -61,11 +60,52 @@ function rewireStylus(config, env) {
 module.exports = function override(config, env) {
   config = rewireStylus(config, env)
   config.plugins = [ ...config.plugins, new DashboardPlugin() ]
+
   config = injectBabelPlugin('loadable-components/babel', config)
+  config = injectBabelPlugin(
+    [
+      'transform-imports',
+      {
+        lodash: {
+          transform        : 'lodash/${member}',
+          preventFullImport: true
+        },
+        'react-router-dom': {
+          transform        : 'react-router-dom/${member}',
+          preventFullImport: true
+        }
+      }
+    ],
+    config
+  )
 
   config.resolve.alias = {
     ...config.resolve.alias,
-    ...aliases
+    ...aliases,
+    'lodash-es': 'lodash'
+  }
+
+  if(env === 'production') {
+    console.log('⚡ Production')
+    config = injectBabelPlugin('transform-react-remove-prop-types', config)
+    config = rewireVendorSplitting(config, env)
+    config.entry = { main: paths.appIndexJs }
+    config.plugins[3] = new UglifyJsPlugin({
+      exclude: /node_modules\/liquidjs\//
+    })
+    config.plugins = [
+      ...config.plugins,
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false
+      })
+    ]
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react      : 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
   }
 
   return config
